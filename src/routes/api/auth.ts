74e7ffdbf98906dc0a62405e12fdf0330c8c0e6f
akulsr0 <akulsr0@gmail.com>
2020-11-09 13:26:37 +0530

    feat: added auth middleware to route

--- src/routes/api/auth.ts
+++ src/routes/api/auth.ts
@@ -1,14 +1,18 @@
 import { Router } from 'express';
 
+// Controllers
 import {
   getCurrentUser,
   registerUser,
   loginUser,
 } from '../../controllers/auth.controller';
 
+// Middlewares
+import { auth } from '../../middlewares/auth.middleware';
+
 const router: Router = Router();
 
-router.route('/').get(getCurrentUser).post(registerUser);
+router.route('/').get(auth, getCurrentUser).post(registerUser);
 router.route('/login').post(loginUser);
 
 module.exports = router;
