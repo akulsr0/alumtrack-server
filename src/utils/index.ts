a832e67f7ecdbfe12477498fa2a7ba3f262ae620
akulsr0 <akulsr0@gmail.com>
2020-11-10 00:44:48 +0530

    feat: added generateVerificationCode util func

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,10 @@
 export const JWT_SECRET = process.env.JWT_SECRET || 'SECRET';
+
+export function generateVerificationCode(n: number): string {
+  var digits = '0123456789';
+  let OTP = '';
+  for (let i = 0; i < n; i++) {
+    OTP += digits[Math.floor(Math.random() * 10)];
+  }
+  return OTP;
+}
